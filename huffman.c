2d97e48f40555198328bc57fdbb5090c93e82127
Stharley <sth4rley@gmail.com>
2023-02-04 23:59:21 -0300

    lista ordenada -> fila de prioridade (freq). e etc

--- huffman.c
+++ huffman.c
@@ -8,17 +8,19 @@
 typedef struct node
 {
     unsigned char caracter;
-    unsigned int freq;
+    unsigned int freq; // a frequencia sera a prioridade da fila de prioridade
     struct node *next;
     struct node *right;
     struct node *left;
 } NODE;
 
-typedef struct
+// Estrutura da fila de prioridade
+typedef struct priority_queue
 {
-    NODE *inicio;
-    unsigned int tam;
-} LISTA;
+    NODE *front;
+    unsigned int size;
+} PriorityQueue;
+
 
 // 1 -- tabela de frequencia
 
@@ -45,81 +47,73 @@ void imprime_tab_frequencia(unsigned int *t)
 }
 
 
-// 2 -- lista encadeada ordenada
+// 2 -- fila de prioridade ordenada
 
-// cria a lista
-void criar_lista(LISTA *L)
+// inicializa a fila
+void init_queue(PriorityQueue *queue)
 {
-    L->inicio = NULL;
-    L->tam = 0;
+    queue->front = NULL;
+    queue->size = 0;
 }
 
-// insere ordenado na lista encadeada
-void inserir_ordenado(LISTA *L, NODE *N)
+// Verifica se a fila está vazia
+int is_empty(PriorityQueue *queue)
 {
-    // lista nao vazia
-    if (L->inicio)
+    return queue->front == NULL;
+}
+
+// Insere um nó na fila de prioridade
+void enqueue(PriorityQueue *queue, NODE *node)
+{
+
+    // Se a fila estiver vazia
+    if (is_empty(queue))
     {
-        // tem frequencia menor do que o inicio da lista (novo primeiro)
-        if (N->freq < L->inicio->freq)
-        {
-            N->next = L->inicio;
-            L->inicio = N;
-        }
-        // tem frequencia maior do que o inicio da lista (insercao no meio ou fim)
-        else
-        {
-            NODE *aux = L->inicio;
-            // substituir abaixo por aux e aux->next
-            while (aux->next && aux->next->freq < N->freq)
-                aux = aux->next;
-            N->next = aux->next;
-            aux->next = N;
-        }
+        queue->front = node;
     }
-    // lista vazia
+
+    // Se o novo nó tiver prioridade menor que o primeiro elemento da fila
+    else if (node->freq < queue->front->freq)
+    {
+        node->next = queue->front;
+        queue->front = node;
+    }
+
+    // Se o novo nó tiver prioridade maior que o primeiro elemento da fila
     else
     {
-        L->inicio = N;
+        NODE *p = queue->front;
+        while (p->next && p->next->freq < node->freq)
+            p = p->next;
+        node->next = p->next;
+        p->next = node;
     }
-    L->tam++;
+
+    queue->size++;
 }
 
-// preenche a lista com os caracteres e suas frequencias
-void preencher_lista(unsigned int *tabela, LISTA *L)
+// preenche a fila com os caracteres que aparecem no texto
+void fill_queue(unsigned int *tabela, PriorityQueue *queue)
 {
-    int i;
-    NODE *new;
-    for (i = 0; i < TAM; i++)
+    for (int i = 0; i < TAM; i++)
     {
         if (tabela[i] > 0)
         {
-            new = (NODE *)malloc(sizeof(NODE));
-            if (new)
-            {
-
-                new->caracter = i;
-                new->freq = tabela[i];
-                new->right = NULL;
-                new->left = NULL;
-                new->next = NULL;
-                inserir_ordenado(L, new);
-            }
-            else
-            {
-                printf("\tErro ao alocar memoria em prencher_lista()!\n");
-                break;
-            }
+            NODE *new = (NODE *)malloc(sizeof(NODE));
+            new->caracter = i;
+            new->freq = tabela[i];
+            new->right = new->left = new->next = NULL;
+            enqueue(queue, new);
         }
     }
 }
 
-// imprime a lista
-void print_list(LISTA *L)
+// imprime a fila de prioridade (apenas para debug)
+void print_queue(PriorityQueue *queue)
 {
-    NODE *aux = L->inicio;
-    printf("Imprimindo Lista!\n");
-    printf("Tamanho: %d\n", L->tam);
+    NODE *aux = queue->front;
+    printf("Imprimindo Fila!\n");
+    printf("Tamanho: %d\n", queue->size);
     unsigned short int count = 0;
     while (aux)
     {
@@ -129,51 +123,46 @@ void print_list(LISTA *L)
 }
 
 
-// 3 -- Montagem da arvore a partir da fila/lista ordenada
+// 3 -- Montagem da arvore a partir da fila de prioridade
 
-// remove o primeiro elemento da lista 
-NODE *remove_no_inicio(LISTA *L)
+// Remove o primeiro elemento da fila de prioridade e retorna o nó
+NODE *dequeue(PriorityQueue *queue)
 {
+    // Se a fila estiver vazia retorna NULL
     NODE *aux = NULL;
 
-    if (L->inicio)
+    // Se a fila não estiver vazia
+    if (queue->front)
     {
-        aux = L->inicio;
-        L->inicio = aux->next;
+        // Remove o primeiro elemento da fila
+        aux = queue->front;
+        queue->front = aux->next;
         aux->next = NULL;
-        L->tam--;
+        queue->size--;
     }
 
+    // Retorna o nó removido
     return aux;
 }
 
-// monta a arvore a partir da lista ordenada
-NODE *montar_arvore(LISTA *lista)
+// monta a arvore a partir da fila de prioridade
+NODE *montar_arvore(PriorityQueue *queue)
 {
     NODE *primeiro, *segundo, *novo;
-    while (lista->tam > 1)
+    while (queue->size > 1)
     {
-        primeiro = remove_no_inicio(lista);
-        segundo = remove_no_inicio(lista);
+        // remove os dois primeiros elementos da fila de prioridade e os armazena em primeiro e segundo
+        primeiro = dequeue(queue);
+        segundo = dequeue(queue);
+        // cria um novo nó e o insere na fila de prioridade ordenada de acordo com a sua frequencia (prioridade)
         novo = malloc(sizeof(NODE));
-
-        if (novo)
-        {
-            novo->caracter = '+';
-            novo->freq = primeiro->freq + segundo->freq;
-            novo->left = primeiro;
-            novo->right = segundo;
-            novo->next = NULL;
-
-            inserir_ordenado(lista, novo);
-        }
-        else
-        {
-            printf("\n\tERRO ao alocar memoria em montar_arvore!\n");
-            break;
-        }
+        novo->freq = primeiro->freq + segundo->freq;
+        novo->left = primeiro;
+        novo->right = segundo;
+        novo->next = NULL;
+        enqueue(queue, novo);
     }
-    return lista->inicio;
+    return queue->front;
 }
 
 // imprime a arvore
@@ -187,6 +176,7 @@ void imprimir_arvore(NODE *raiz, int tam)
         imprimir_arvore(raiz->right, tam + 1);
     }
 }
+
 
 // ------------------  PARTE 4: Montar dicionario ------------------
 
@@ -210,6 +200,7 @@ int altura_arvore(NODE *raiz)
 // aloca o dicionario
 char **aloca_dicionario(int colunas)
 {
+    // aloca a matriz de strings (dicionario) e retorna o ponteiro para a primeira linha da matriz
     char **dicionario = malloc(sizeof(char *) * TAM);
     for (int i = 0; i < TAM; i++)
     {
@@ -221,11 +212,17 @@ char **aloca_dicionario(int colunas)
 // gera o dicionario a partir da arvore
 void gerar_dicionario(char **dicionario, NODE *raiz, char *caminho, int colunas)
 {
+    // cria duas strings para armazenar os caminhos da esquerda e da direita
     char esquerda[colunas], direita[colunas];
+
+    // se o nó for uma folha (não tem filhos) adiciona o caminho no dicionario
     if (raiz->left == NULL && raiz->right == NULL)
     {
         strcpy(dicionario[raiz->caracter], caminho);
     }
+
+    // se o nó não for uma folha, chama a função recursivamente para os filhos da esquerda e da direita 
+    // concatenando o caminho com 0 ou 1 para indicar a direção do caminho (esquerda ou direita)
     else
     {
         strcpy(esquerda, caminho);
@@ -275,7 +272,6 @@ char *codificar(char **dicionario, unsigned char *texto)
 // --------------------- PARTE 6: Decodificacao ----------------------
 
 // retorna o texto decodificado usando a arvore passada como parametro
-
 char *decodificar(unsigned char *codigo, NODE *raiz)
 {
     NODE *aux = raiz;                                          // auxiliar para percorrer a arvore
@@ -299,10 +295,12 @@ char *decodificar(unsigned char *codigo, NODE *raiz)
     return decodificado; // retorna o texto decodificado
 }
 
+
 // ------------------- PARTE 7 : Compactar ----------------
 
 void compactar(unsigned char *str)
 {
+
     int i = 0, j = 7;
     unsigned char mascara, byte = 0;
     FILE *file = fopen("compactado.cpt", "wb");
@@ -320,7 +318,10 @@ void compactar(unsigned char *str)
             j--;
 
             if (j < 0)
-            { // depois de manipular o byte de forma a formar o codigo parecido com o da string. Em seguida, mandamos o byte formado para o arquivo e reiniciamos o processo em busca de gerar o proximo byte.
+            {
+                // depois de manipular o byte de forma a formar o codigo parecido com o da string.
+                // Em seguida, mandamos o byte formado para o arquivo e reiniciamos
+                // o processo em busca de gerar o proximo byte.
                 fwrite(&byte, sizeof(unsigned char), 1, file);
 
                 byte = 0;
@@ -438,15 +439,15 @@ int main(void)
     preenche_tab_frequencia(&tabela_frequencia, text);
     imprime_tab_frequencia(&tabela_frequencia);
 
-    // 2 -- lista encadeada ordenada
-    LISTA list;
-    criar_lista(&list);
-    preencher_lista(tabela_frequencia, &list);
-    print_list(&list);
+    // 2 -- fila de prioridade
+    PriorityQueue queue;
+    init_queue(&queue);
+    fill_queue(tabela_frequencia, &queue);
+    print_queue(&queue);
 
-    // 3 -- Montagem da arvore a partir da lista/fila ordenada
+    // 3 -- Montagem da arvore a partir da fila de prioridade
     NODE *arvore;
-    arvore = montar_arvore(&list);
+    arvore = montar_arvore(&queue);
     printf("\n\tImprimindo arvore de huffman\n");
     imprimir_arvore(arvore, 0);
 
@@ -464,7 +465,8 @@ int main(void)
     decodificado = decodificar(codificado, arvore);
     printf("\n\tTexto decodificado: %s\n", decodificado);
 
-    /*até aqui codificamos em formato de string, a ideia agora é transformar uma cadeia de 8 caractéres em um unico byte*/
+    /*até aqui codificamos em formato de string, a ideia agora é transformar uma
+    cadeia de 8 caractéres em um unico byte*/
 
     // 7 -- Criar arquivo COMPACTADO
     // compactar(codificado);
